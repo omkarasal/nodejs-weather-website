0eda923e72fa6224f08c0264c004e5e63c5213c8
Rasal <2328160@cognizant.com>
2025-11-20 17:09:39 +0530

    Setup app for heroku

--- public/js/script.js
+++ public/js/script.js
@@ -12,7 +12,7 @@ weatherForm.addEventListener('submit', e => {
     weatherInfo.textContent = 'Loading...';
     errorMessage.textContent = '';
 
-    fetch("http://localhost:3000/weather?address="+location).then(response => {
+    fetch("/weather?address="+location).then(response => {
         response.json().then(data => {
             if (data.error) {
                 errorMessage.textContent = data.error;
