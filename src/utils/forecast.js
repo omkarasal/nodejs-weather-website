301500bcce8dfc18c06da88c51aa82a44f72c063
Rasal <2328160@cognizant.com>
2025-11-20 17:28:15 +0530

    Add humidity data to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -11,7 +11,8 @@ const forecast = (latitude, longitude, callback) => {
         } else {
             callback(undefined, body.current.weather_descriptions[0] + ". It is currently " + 
                 body.current.temperature + " degrees out. It feels like " + 
-                body.current.feelslike + " degrees out."
+                body.current.feelslike + " degrees out. Humidity is " + 
+                body.current.humidity + "%."
             );
         }
     });
